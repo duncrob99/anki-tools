58f0f3fe200488a54b54b548531617c16e341a44
Duncan Robertson <duncrob99@gmail.com>
2023-03-04 09:26:03 +1100

    Added default storage of typedans

--- storage.js
+++ storage.js
@@ -1,40 +1,53 @@
-function loadStorage() {
-  window.storage = window.storage ?? {};
-  try {
-    console.log(sessionStorage);
-    storage = sessionStorage;
-  } catch {
-    function refreshLength() {
-      storage.length = Object.keys(customStorage).length;
-    }
+(function() {
+  function loadStorage() {
+    window.storage = window.storage ?? {};
+    try {
+      console.log(sessionStorage);
+      storage = sessionStorage;
+    } catch {
+      function refreshLength() {
+        storage.length = Object.keys(customStorage).length;
+      }
 
-    console.log('no session storage available');
-    window.customStorage = window.customStorage ?? {};
-    refreshLength();
-
-    storage.setItem = function(key, value) {
-      customStorage[key] = value;
+      console.log('no session storage available');
+      window.customStorage = window.customStorage ?? {};
       refreshLength();
-    }
 
-    storage.clear = function() {
-      customStorage = {};
-      refreshLength();
-    }
+      storage.setItem = function(key, value) {
+        customStorage[key] = value;
+        refreshLength();
+      }
 
-    storage.getItem = function(key) {
-      return customStorage[key];
-    }
+      storage.clear = function() {
+        customStorage = {};
+        refreshLength();
+      }
 
-    storage.key = function(index) {
-      return Object.keys(customStorage)[index];
-    }
-    
-    storage.removeItem = function(key) {
-      delete customStorage[key];
-      refreshLength();
+      storage.getItem = function(key) {
+        return customStorage[key];
+      }
+
+      storage.key = function(index) {
+        return Object.keys(customStorage)[index];
+      }
+      
+      storage.removeItem = function(key) {
+        delete customStorage[key];
+        refreshLength();
+      }
     }
   }
-}
 
-loadStorage();
+  loadStorage();
+
+  let default_input = document.querySelector('input#typeans');
+  if (default_input) {
+    default_input.addEventListener('input', function() {
+      storage.setItem('typed_ans', default_input.value);
+    });
+  }
+
+  window.getTypedAns = function() {
+    return storage.getItem('typed_ans');
+  }
+})();
